018870e850cbf761b9034a1ae4e008fe03c2335d
luhobrecht <luhobrecht@hotmail.com>
2022-01-27 20:05:16 +0100

    Agrego ejercicios hasta clase 14

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-const express = require("express");
-
+const express = require('express');
 const app = express();
-
-const path = require("path");
-
-//const publicPath = path.join(__dirname, "./public");
-
-//app.use(express.static(publicPath));
-
 app.use(express.static('public'));
 
-
-app.listen(3000, () => {
-     console.log("Funciona todo bien");
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
 });
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/home.html"));
-})
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
